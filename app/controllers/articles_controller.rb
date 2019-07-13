150e03f53b767ea553c48e146956dbeb803259ca
kuchlong kuchlong <kuchlong@gamil.com>
2019-07-13 05:35:25 +0000

    add edit and update methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
     @article = Article.new
     
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
   def create
     @article = Article.new(article_params
     )
@@ -19,6 +22,15 @@ class ArticlesController < ApplicationController
     render 'new'
   end
   end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+      else
+        render 'edit'
+    end
+  end 
   
   def show
     
