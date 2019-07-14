5f3189788a965e33520a901c5e706f4d09e327cb
kuchlong kuchlong <kuchlong@gamil.com>
2019-07-14 06:08:28 +0000

    added comments controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+   http_basic_authenticate_with name: "kuch", password: "hiromi", except: [:index, :show]
+   
   def index 
     @articles = Article.all
   end
