5ae10dab1409c56b7251d17e9aed7b794cab98ab
kuchlong kuchlong <kuchlong@gamil.com>
2019-07-14 04:18:09 +0000

    refactor duplicate code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,10 +37,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "article was sucessfully destroyed"
+    
+    redirect_to  articles_path
+  end
+  
   private
   def article_params
     
     params.require(:article).permit(:title, :text)
   end
 end
- 
+  
